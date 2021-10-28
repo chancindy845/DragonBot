9f78ef7b4dffdbf40ab4f6e48727ebc997746371
tukibdma@gmail.com <91396589+chancindy845@users.noreply.github.com>
2021-10-28 18:48:04 -0500

    Update help.js

--- src/help.js
+++ src/help.js
@@ -37,58 +37,58 @@ Comando de bienvenida solo grupos
 
 
 ◉ *CREAR STICKERS*
-  ║
-  ╠ ○ ${prefix}sticker
-  ╠ ○ ${prefix}attp
-  ╠ Mas un texto corto
-  ╠ ○ ${prefix}stickergif
-  ╚ 6 segundos de video
+  ┣❑
+  ┣❑ ○ ${prefix}sticker
+  ┣❑ ○ ${prefix}attp
+  ┣❑ Mas un texto corto
+  ┣❑ ○ ${prefix}stickergif
+  ┣❑ 6 segundos de video
 
 ◉ *CONVERTIDORES*
-  ║
-  ╠ ○ ${prefix}toimg
-  ╠ De sticker a JPG
-  ╠ ○ ${prefix}tomp3
-  ╚ De video a MP3
+  ┣❑
+  ┣❑ ○ ${prefix}toimg
+  ┣❑ De sticker a JPG
+  ┣❑ ○ ${prefix}tomp3
+  ┣❑ De video a MP3
 
 ◉ *AUDIO*
-  ║
-  ╠ ○ ${prefix}idioma
-  ╚ ○ ${prefix}tts es (mas texto)
+  ┣
+  ┣❑ ○ ${prefix}idioma
+  ┣❑ ○ ${prefix}tts es (mas texto)
 
 ◉ *OTROS*
-  ║
-  ╠ ○ ${prefix}wame
-  ╠ Link de Whatsapp
-  ╠ ○ ${prefix}qrcode
-  ╚ Coloca un texto
+  ┣
+  ┣❑ ○ ${prefix}wame
+  ┣❑ Link de Whatsapp
+  ┣❑ ○ ${prefix}qrcode
+  ┣❑ Coloca un texto
 
 ◉ *GRUPOS*
-  ║
-  ╠ ○ ${prefix}unir
-  ╠ Unir a una persona al grupo
-  ╠ ○ ${prefix}fgc
-  ╠ Cambiar foto del grupo
-  ╠ ○ ${prefix}ngc
-  ╠ Cambiar nombre del grupo
-  ╠ ○ ${prefix}dgc
-  ╠ Cambiar descripcion del grupo
-  ╠ ○ ${prefix}closegc
-  ╠ Cerrar el grupo solo admins
-  ╠ ○ ${prefix}opengc
-  ╠ Abrir grupo solo admins
-  ╠ ○ ${prefix}kick
-  ╠ Eliminar a un miembro 
-  ╠ ○ ${prefix}promote
-  ╠ Dar admin a un miembro
-  ╠ ○ ${prefix}demote
-  ╠ Quitar el admin
-  ╠ ○ ${prefix}linkgc
-  ╠ Link del grupo
-  ╠ ○ ${prefix}adminlist
-  ╠ Nombra a los admins del grupo
-  ╠ ○ ${prefix}todos
-  ╚ Nombra a todos los del grupo
+  ┣
+  ┣❑ ○ ${prefix}unir
+  ┣❑ Unir a una persona al grupo
+  ┣❑ ○ ${prefix}fgc
+  ┣❑ Cambiar foto del grupo
+  ┣❑ ○ ${prefix}ngc
+  ┣❑ Cambiar nombre del grupo
+  ┣❑ ○ ${prefix}dgc
+  ┣❑ Cambiar descripcion del grupo
+  ┣❑ ○ ${prefix}closegc
+  ┣❑ Cerrar el grupo solo admins
+  ┣❑ ○ ${prefix}opengc
+  ┣❑ Abrir grupo solo admins
+  ┣❑ ○ ${prefix}kick
+  ┣❑ Eliminar a un miembro 
+  ┣❑ ○ ${prefix}promote
+  ┣❑ Dar admin a un miembro
+  ┣❑ ○ ${prefix}demote
+  ┣❑ Quitar el admin
+  ┣❑ ○ ${prefix}linkgc
+  ┣❑ Link del grupo
+  ┣❑ ○ ${prefix}adminlist
+  ┣❑ Nombra a los admins del grupo
+  ┣❑ ○ ${prefix}todos
+  ┣❑ Nombra a todos los del grupo
 
 Para usar estas funciones el bot necesita admin
   
